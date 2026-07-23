75db81423b7859e2bf63eb2fb7dba9ca99fb6a87
MuhammadMudassarYounas <mudassaryounas362@gmail.com>
2026-07-23 22:53:20 +0500

    Update ValidationError

--- backend/src/common/errors/ValidationError.ts
+++ backend/src/common/errors/ValidationError.ts
@@ -1,5 +1,5 @@
 import { ApiError } from "./ApiError";
-
+// This class represents a "Validation" error, which is a specific type of API error that occurs when the input data fails validation checks.
 export class ValidationError extends ApiError {
   constructor(message = "Validation Error") {
     super(400, message);
