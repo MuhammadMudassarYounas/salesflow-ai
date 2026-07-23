68f94a6532d2ab82c65fa67ebb608699777b318b
MuhammadMudassarYounas <mudassaryounas362@gmail.com>
2026-07-23 22:48:23 +0500

    feat: implement customer CRUD

--- backend/src/common/errors/NotFoundError.ts
+++ backend/src/common/errors/NotFoundError.ts
@@ -1,5 +1,5 @@
 import { ApiError } from "./ApiError";
-
+// This class represents a "Not Found" error, which is a specific type of API error that occurs when a requested resource cannot be found.
 export class NotFoundError extends ApiError {
   constructor(message = "Resource not found") {
     super(404, message);
